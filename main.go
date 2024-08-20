9fd2488fd917ac10f0d2968ed36b7dfec6e9da5f
Goyal, Koshal <ar-koshal.goyal@rakuten.com>
2024-08-20 19:47:20 +0530

    tutorial complete

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var conferenceName = "My Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+	fmt.Printf("hello welcome to conference %v total tickets = %v remaining tickets = %v", conferenceName, conferenceTickets, remainingTickets)
+	
+
+}
